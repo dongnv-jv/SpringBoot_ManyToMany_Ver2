ddbb9ea09d1c6e0415f0575128fe6671e63eccae
dongbk97 <dongtr2202@gmail.com>
2022-09-10 09:25:27 +0700

    Create project

---
+++ src/main/java/com/example/springboot_manytomany_ver2/SpringBootManyToManyVer2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_manytomany_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManyToManyVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootManyToManyVer2Application.class, args);
+    }
+
+}
